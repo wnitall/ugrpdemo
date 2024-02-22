d43d605236002c10adeaedaedf127a75840f2e61
minsu <minsu010806@dgiat.ac.kr>
2024-02-22 19:23:26 +0900

    [first demo] 1. video backward is not implemented 2. need optimization

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,235 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Shorts',
+      home: HomeScreen(),
+    );
+  }
 }
 
-//stless + tap 누르면 기본 세팅
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class HomeScreen extends StatefulWidget {
+  @override
+  HomeScreenState createState() => HomeScreenState();
+}
+
+class HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+  static final List<Widget> _widgetOptions = <Widget>[
+    ShortsScreen(),
+    UserProfileScreen(),
+    Text('지도 화면'),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp( //MaterialApp은 구글 스타일(각종 기능, 디자인 지원) ios 스타일은 cupertino~~
-      home : Scaffold(
-        appBar: AppBar(
-          title: Text("앱임"),
-          backgroundColor: Colors.blue,
-        ),
-        body: Align(
-          alignment: Alignment.bottomLeft,
-          child: Container(
-            width: 200, height: 100,
-            margin: EdgeInsets.all(20),
-            padding: EdgeInsets.all(20),
-            child: Text("dedede"),
-            decoration: BoxDecoration(
-              color: Colors.blue,
-              border: Border.all(color: Colors.black)
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Shorts'),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '쇼츠',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: '계정',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map),
+            label: '지도',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
+
+class ShortsScreen extends StatefulWidget {
+  @override
+  _ShortsScreenState createState() => _ShortsScreenState();
+}
+
+class _ShortsScreenState extends State<ShortsScreen> {
+  late List<VideoPlayerController> _controllers = [];
+  late PageController _pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    // Dummy video list. 실제 앱에서는 동적으로 리스트를 구성해야 합니다.
+    _pageController = PageController();
+    _controllers = [
+      VideoPlayerController.asset('assets/videos/video1.mp4')
+        ..initialize().then((_) {
+          setState(() {});
+          _controllers[0].play();
+        }),
+      VideoPlayerController.asset('assets/videos/video2.mp4')
+        ..initialize().then((_) {
+          setState(() {});
+          _controllers[1].play();
+        }),
+      VideoPlayerController.asset('assets/videos/video3.mp4')
+        ..initialize().then((_) {
+          setState(() {});
+          _controllers[2].play();
+        }),
+      VideoPlayerController.asset('assets/videos/video4.mp4')
+        ..initialize().then((_) {
+          setState(() {});
+          _controllers[3].play();
+        }),
+      VideoPlayerController.asset('assets/videos/video5.mp4')
+        ..initialize().then((_) {
+          setState(() {});
+          _controllers[4].play();
+        }),
+      VideoPlayerController.asset('assets/videos/video6.mp4')
+        ..initialize().then((_) {
+          setState(() {});
+          _controllers[5].play();
+        }),
+      // 더 많은 동영상을 추가하세요.
+    ];
+
+    _controllers.forEach((controller) {
+      controller.setLooping(true);
+    });
+  }
+
+  @override
+  void dispose() {
+    _controllers.forEach((controller) {
+      controller.dispose();
+    });
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView.builder(
+      controller: _pageController,
+      scrollDirection: Axis.vertical,
+      itemCount: _controllers.length,
+      itemBuilder: (context, index) {
+        return VideoPlayerScreen(controller: _controllers[index]);
+      },
+    );
+  }
+}
+
+class VideoPlayerScreen extends StatelessWidget {
+  final VideoPlayerController controller;
+
+  VideoPlayerScreen({required this.controller});
+
+  @override
+  Widget build(BuildContext context) {
+    return controller.value.isInitialized
+        ? GestureDetector(
+      onTap: () {
+        controller.value.isPlaying ? controller.pause() : controller.play();
+      },
+      child: AspectRatio(
+        aspectRatio: controller.value.aspectRatio,
+        child: VideoPlayer(controller),
+      ),
+    )
+        : Center(child: CircularProgressIndicator());
+  }
+}
+
+class UserProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('사용자 계정'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.settings),
+            onPressed: () {
+              // 설정 화면 이동 로직 추가
+            },
+          ),
+        ],
+      ),
+      body: ListView(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 40.0,
+                  backgroundImage: NetworkImage('https://via.placeholder.com/150'),
+                ),
+                Expanded(
+                  child: Column(
+                    children: <Widget>[
+                      Text('게시물 수', style: TextStyle(fontWeight: FontWeight.bold)),
+                      Text('100'),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    children: <Widget>[
+                      Text('팔로워', style: TextStyle(fontWeight: FontWeight.bold)),
+                      Text('200K'),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    children: <Widget>[
+                      Text('팔로잉', style: TextStyle(fontWeight: FontWeight.bold)),
+                      Text('300'),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ),
-        )
-      )
+          GridView.count(
+            crossAxisCount: 3,
+            crossAxisSpacing: 4.0,
+            mainAxisSpacing: 4.0,
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            children: List.generate(30, (index) {
+              return Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage('https://via.placeholder.com/150'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              );
+            }),
+          ),
+        ],
+      ),
     );
   }
 }
